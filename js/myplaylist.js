3837aa004d1907eb5a6249b037148f8d412c0a9c
Jhulianc3 <jhulianc3@gmail.com>
2017-06-02 13:51:08 -0300

    Enviei os arquivos pela primeira vez

---
+++ js/myplaylist.js
@@ -0,0 +1,63 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/ArianaGrande_Love_Me_Harder.mp3',
+        title:'Love Me Harder',
+        artist:'Ariana Grande',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:56',
+        cover:'mix/Ariana.png'
+    },
+
+    {
+        mp3:'mix/50Cent_PIMP.mp3',
+        title:'P.I.M.P',
+        artist:'50Cent',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:08',
+        cover:'mix/50.png'
+    },
+    {
+        mp3:'mix/AvrilLavigne_Im With_You.mp3',
+        title:'Im With You',
+        artist:'Avril Lavigne',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:44',
+        cover:'mix/Avril.png'
+    },
+    {
+        mp3:'mix/Colbie_Caillat_Bubbly.mp3',
+        title:'Bubbly',
+        artist:'Colbie Caillat',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:14',
+        cover:'mix/Colbie.jpg'
+    
+    },
+    {
+        mp3:'mix/WizKhalifa_We_Dem_Boyz.mp3',
+        title:'We Dem Boyz',
+        artist:'Wiz Khalifa',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:45',
+        cover:'mix/Wiz.jpg'
+    },
+];
